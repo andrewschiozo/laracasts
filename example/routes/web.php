3eaf41e426f3a298b5e279acc0b47935b228ebb6
Andrews Chiozo <andrewschiozo@gmail.com>
2025-03-20 15:55:35 -0300

    feat: novas rotas para testes

--- example/routes/web.php
+++ example/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Return string
+Route::get('/string', function () {
+    return 'Foo, bar';
+});
+
+//Return array
+Route::get('/array', function () {
+    return ['foo' => 'bar'];
+});
+
+//Return about page
+Route::get('/about', function () {
+    return view('about');
+});
+
+//Return contact page
+Route::get('/contact', function () {
+    return view('contact');
+});
