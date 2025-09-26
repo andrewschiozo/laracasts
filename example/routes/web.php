0c3a6d27a8cefe1635110825db7ed6a8514a92f4
Andrews <andrewschiozo@gmail.com>
2025-09-25 23:35:46 -0300

    feat: nomes para as rotas

--- example/routes/web.php
+++ example/routes/web.php
@@ -8,17 +8,17 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
 
 //Return about page
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 //Return contact page
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 //Return string
 Route::get('/string', function () {
