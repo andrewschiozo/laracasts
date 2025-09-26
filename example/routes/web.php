80fbe894b6e97c4d03e42800cd759816cc278e50
Andrews <andrewschiozo@gmail.com>
2025-09-25 21:52:29 -0300

    fix: redirecionamento da rota / para /home

--- example/routes/web.php
+++ example/routes/web.php
@@ -3,17 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
     return view('home');
-});
-
-//Return string
-Route::get('/string', function () {
-    return 'Foo, bar';
-});
-
-//Return array
-Route::get('/array', function () {
-    return ['foo' => 'bar'];
 });
 
 //Return about page
@@ -24,4 +18,14 @@ Route::get('/about', function () {
 //Return contact page
 Route::get('/contact', function () {
     return view('contact');
+});
+
+//Return string
+Route::get('/string', function () {
+    return 'Foo, bar';
+});
+
+//Return array (JSON)
+Route::get('/array', function () {
+    return ['foo' => 'bar'];
 });
