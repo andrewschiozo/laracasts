3590b49f73d884aed7f1ed581feb3346fff7d52c
Andrews <andrewschiozo@gmail.com>
2025-09-25 23:37:24 -0300

    refactor: uso de props :active

--- example/resources/views/Components/menu-link.blade.php
+++ example/resources/views/Components/menu-link.blade.php
@@ -1,1 +1,12 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+@props(['active' => false, 'device' => 'desktop'])
+
+{{-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --}}
+@php($classActive = $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white')
+@php($classDevice = $device === 'mobile' ? 'block' : '')
+
+<a class="{{ $classDevice }} {{ $classActive }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+   {{ $attributes }}
+>
+    {{ $slot }}
+</a>
--- example/resources/views/Components/menu.blade.php
+++ example/resources/views/Components/menu.blade.php
@@ -1,18 +1,15 @@
-@props(['device'])
+@props(['device' => 'desktop'])
 @php($menus = json_decode('[{"name": "Home", "url": "home"}, {"name": "About", "url": "about"}, {"name": "Contact", "url": "contact"}]'))
-@php($classNavDevice = $device === 'mobile' ? 'space-y-1 px-2 pt-2 pb-3 sm:px-3' : '')
 
-<nav {{ $attributes }} class="{{ $classNavDevice }}">
-    {{-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --}}
-    @php($classDevice = $device === 'mobile' ? 'block' : '')
+<nav class="{{ $device === 'mobile' ? 'space-y-1 px-2 pt-2 pb-3 sm:px-3' : '' }}"
+    {{ $attributes }}
+>
     @foreach($menus as $menu)
-        @php($classActive = request()->is($menu->url) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white')
-        @php($ariaCurrent = request()->is($menu->url) ? 'page' : '')
-
         <x-menu-link
-            class="{{ $classDevice }} {{ $classActive }} rounded-md px-3 py-2 text-sm font-medium"
             href="/{{ $menu->url }}"
-            aria-current="{{ $ariaCurrent }}">
+            :active="request()->routeIs($menu->url)"
+            device="{{$device}}"
+        >
             {{ $menu->name }}
         </x-menu-link>
     @endforeach
