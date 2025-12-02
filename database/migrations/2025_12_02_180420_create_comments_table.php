59517a7b13f94f3ebc3470f6d4e4e1a5f8d28536
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-12-02 15:32:19 -0300

    Model Factories

--- database/migrations/2025_12_02_180420_create_comments_table.php
+++ database/migrations/2025_12_02_180420_create_comments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,6 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('comment');
+            $table->foreignIdFor(Post::class, 'post_id');
             $table->foreignIdFor(User::class, 'author_id');
             $table->timestamps();
         });
