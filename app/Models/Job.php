d084fcc77f1396cf882835e1ba3a76690f673f36
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-12-02 15:10:13 -0300

    Meet Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,25 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'Director', 'salary' => 'R$ 10.000,00'],
-            ['id' => 2, 'title' => 'Developer', 'salary' => 'R$ 8.000,00'],
-            ['id' => 3, 'title' => 'Designer', 'salary' => 'R$ 6.000,00']
-        ];
-    }
+class Job extends Model{
 
-    public static function find($id): array
-    {
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
-        if(!$job) {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ Route::get('/home', function () {
 })->name('home');
 
 Route::get('/jobs', function () {
+//     Job::create(['title' => 'Director', 'salary' => 'R$ 10.000,00']);
+//     Job::create(['title' => 'Developer', 'salary' => 'R$ 8.000,00']);
+//     Job::create(['title' => 'Designer', 'salary' => 'R$ 6.000,00']);
     return view('jobs', [
         'jobs' => Job::all()
     ]);
