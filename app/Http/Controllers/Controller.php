2f9a0be221a87a209a4c9c2a994792f40bee9ee0
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-12-09 13:41:07 -0300

    feat: extends basecontroller and use authorizes requests

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Barryvdh\Debugbar\Controllers\BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests;
 }
